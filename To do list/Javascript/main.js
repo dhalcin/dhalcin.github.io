ff2a03cf48d28ae675cb912161c721781d98ace1
dhalcin <hankgit-h@hotmail.com>
2023-07-18 23:01:33 -0500

    handing incoming tasks

--- To do list/Javascript/main.js
+++ To do list/Javascript/main.js
@@ -1,18 +1,24 @@
 
-
 let count = 0;
 
+const input = document.getElementById('inpt');
 const button = document.getElementById('btn');
 const listtasks = document.getElementById('list');
 
 button.addEventListener('click', e => {
+    count++;
     e.preventDefault();
     const tasks = document.createElement('li');
-    count++;
-    tasks.id = `task${count}`;
+    const btnli = document.createElement('button');
+    
     tasks.textContent = input.value;
     listtasks.appendChild(tasks);
+    tasks.appendChild(btnli);
     input.value = '';
 
+    btnli.addEventListener('click', ()=>{
+        listtasks.removeChild(tasks);
+    });
 
 });
+
