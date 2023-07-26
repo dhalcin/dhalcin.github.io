c9d956112771a279ceef34505a3131cceab11f47
dhalcin <hankgit-h@hotmail.com>
2023-07-25 21:42:04 -0500

    managing the operations

--- Calculator/Javascript/main.js
+++ Calculator/Javascript/main.js
@@ -1,52 +1,113 @@
-const other = document.getElementById('other');
+const ouput = document.getElementById('ouput');
 const input = document.getElementById('inpt');
 const buttons = document.querySelectorAll('button');
 
-other.disabled = true;
+ouput.disabled = true;
+input.value = '0';
 
-function remove() {
-    other.value = '';
-    input.value = '';
+function reset() {
+    ouput.value = '';
+    input.value = '0';
+}
+
+function operations() {
+    let numbers = ouput.value.split(/\+|\-|\=|\*|\÷/g).filter(Boolean);
+    let operators = ouput.value.split(/[0-9]/g).filter(Boolean);
+
+    switch (true) {
+        case operators.includes('+'):
+            input.value = Number(numbers[0]) + Number(numbers[1]);
+            break;
+        case operators.includes('*'):
+            input.value = Number(numbers[0]) * Number(numbers[1]);
+            break;
+        case operators.includes('-'):
+            input.value = Number(numbers[0]) - Number(numbers[1]);
+            break;
+        case operators.includes('÷'):
+            input.value = Number(numbers[0]) / Number(numbers[1]);
+            break;
+    }
+
+    /*if (operators.includes('+')) {
+        input.value = Number(numbers[0]) + Number(numbers[1]);
+    } else if (operators.includes('-')) {
+        input.value = Number(numbers[0]) - Number(numbers[1]);
+    } else if (operators.includes('*')) {
+        input.value = Number(numbers[0]) * Number(numbers[1]);
+        console.log(Number(numbers[0]) * Number(numbers[1]));
+    } else if (operators.includes('÷')) {
+        input.value = Number(numbers[0]) / Number(numbers[1]);
+    }*/
 }
 
 function elements(e) {
     e.preventDefault();
-    
-    if (this.id === 'remove') {
-        remove();
-    } else {
-
-        if (other.value === '' && input.value === '') {
-            switch (this.id) {
-                case 'division':
-                    other.value += '0 ÷';
-                    break;
-                
-                case 'multiply':
-                    other.value += '0 *';
-                    break;
-                
-                case 'substract':
-                    other.value += '0 -';
-                    break;
-                
-                case 'add':
-                    other.value += '0 +';
-                    break;
-                
-                case 'equal':
-                    other.value += '0 =';
-                    break;
-            }
-        } else {
-            input.value += this.innerText;
-        }
-
-
+    switch (this.id) {
+        case 'reset':
+            reset();
+            break;
         
+        case 'division':
+            if (ouput.value[(ouput.value).length -1] === '÷') {
+                ouput.value += '';
+            } else {
+                ouput.value += input.value + this.innerText;
+            }
+            break;
+
+        case 'multiply':
+            if (ouput.value[(ouput.value).length -1] === '*') {
+                ouput.value += '';
+            } else {
+                ouput.value += input.value + this.innerText;
+            }
+            break;
+            
+        case 'add':
+            if (ouput.value[(ouput.value).length -1] === '+') {
+                ouput.value += '';
+            } else {
+                ouput.value += input.value + this.innerText;
+            }
+            break;
+        
+        case 'substract':
+            if (ouput.value[(ouput.value).length -1] === '-') {
+                ouput.value += '';
+            } else {
+                ouput.value += input.value + this.innerText;
+            }
+            break;
+        
+        case 'equal':
+            ouput.value += `${input.value}${this.innerText}`;
+
+            operations();
+
+            break;
+        
+        default:
+            input.value = '';
+            if (input.value === '0') { //?
+                input.value += this.innerText;
+                console.log('primer valor ingresado');
+            } else {
+                input.value += this.innerText;
+            }
+
     }
+
 }
 
 buttons.forEach((button) => {
     button.addEventListener('click', elements);
 });
+
+/*            /*let numbers = ouput.value.split(/\+|\-|\=|\÷/g).filter(Boolean);
+            let operator = ouput.value.split(/[0-9]/g).filter(Boolean);
+            if (operator.includes('÷')) {
+  
+               console.log(Number(numbers[0]) / Number(numbers[1]));
+                input.value = Number(numbers[0]) / Number(numbers[1])
+            }*/ 
