d30cf73ad6f6cee66e86d1fecc943aec59607898
jhon <hankg-h@hotmail.com>
2023-12-26 20:09:57 -0500

    adding books

--- Library/js/library.js
+++ Library/js/library.js
@@ -3,19 +3,44 @@ const add = document.getElementById('add');
 const dialog = document.getElementById('dialog');
 const save = document.getElementById('save');
 const closed = document.getElementById('closed');
+const content = document.getElementsByClassName('content')[0];
+const title = document.getElementById('title-book').value;
+const author = document.getElementById('author').value;
+const pages = document.getElementById('pages').value;
 
-const content = document.getElementsByClassName('content');
-const div = document.createElement('div');
-div.classList.add('book')
+const myLibrary = [];
 
-function closedDialog() {
-    dialog.close();
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    //this.read = read;
+}
+
+function verification(str) {
+    return (str.value).trim() !== '';
+}
+
+function addBookToLibrary() {
+    let book = new Book(title, author, pages);
+    myLibrary.push(book);
+}
+
+function books() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        for (let property in myLibrary[i]) {
+            console.log(`${property} : ${myLibrary[i][property]}`);
+        }
+    }
 }
 
 add.addEventListener('click', ()=> {
-    dialog.showModal();
+    dialog.showModal(); 
 })
 
 save.addEventListener('click', ()=> {
-    content.appendChild(div);
+    if (verification(title) && verification(author) && verification(pages)) {
+        addBookToLibrary(title, author, pages);
+        books();
+    }
 })
