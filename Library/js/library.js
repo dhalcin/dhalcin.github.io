07441edb5e0febff5ca36d16929c9d5c9edc9c2c
jhon <hankg-h@hotmail.com>
2023-12-27 23:40:04 -0500

    adding books

--- Library/js/library.js
+++ Library/js/library.js
@@ -4,9 +4,6 @@ const dialog = document.getElementById('dialog');
 const save = document.getElementById('save');
 const closed = document.getElementById('closed');
 const content = document.getElementsByClassName('content')[0];
-const title = document.getElementById('title-book').value;
-const author = document.getElementById('author').value;
-const pages = document.getElementById('pages').value;
 
 const myLibrary = [];
 
@@ -18,19 +15,32 @@ function Book(title, author, pages) {
 }
 
 function verification(str) {
-    return (str.value).trim() !== '';
+    return (str).trim() !== '';
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages) {
     let book = new Book(title, author, pages);
     myLibrary.push(book);
 }
 
 function books() {
     for (let i = 0; i < myLibrary.length; i++) {
+        let book = document.createElement('div');
+        let yes = document.createElement('button');
+        let not = document.createElement('button');
+
+        book.classList.add('book');
+        
         for (let property in myLibrary[i]) {
-            console.log(`${property} : ${myLibrary[i][property]}`);
+            let p = document.createElement('p');
+            p.textContent = `${property} : ${myLibrary[i][property]}\n`;
+            book.appendChild(p);
+            book.appendChild(yes);
+            book.appendChild(not);
         }
+
+        content.appendChild(book);
+        myLibrary.length = 0;
     }
 }
 
@@ -38,9 +48,21 @@ add.addEventListener('click', ()=> {
     dialog.showModal(); 
 })
 
-save.addEventListener('click', ()=> {
+save.addEventListener('click', (e)=> {
+    e.preventDefault();
+    let title = document.getElementById('title-book').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    
     if (verification(title) && verification(author) && verification(pages)) {
         addBookToLibrary(title, author, pages);
         books();
-    }
+    } 
+
+    dialog.close();
+})
+
+closed.addEventListener('click', e => {
+    e.preventDefault();
+    dialog.close();
 })
