59f0c3b9952061cf75823badff89bd6b4118dd67
jhon <hankg-h@hotmail.com>
2024-01-01 22:52:58 -0500

    adding delete book button

--- Library/js/library.js
+++ Library/js/library.js
@@ -18,29 +18,39 @@ function verification(str) {
     return (str).trim() !== '';
 }
 
+function renderBooks() {
+    content.innerHTML = '';
+    books();
+}
+
 function addBookToLibrary(title, author, pages) {
     let book = new Book(title, author, pages);
     myLibrary.push(book);
+    renderBooks();
 }
 
 function books() {
     for (let i = 0; i < myLibrary.length; i++) {
         let book = document.createElement('div');
-        let yes = document.createElement('button');
-        let not = document.createElement('button');
-
+        let buttonDelet = document.createElement('button');
+        book.dataset.index = i;
+        buttonDelet.textContent = 'Delet';
         book.classList.add('book');
-        
+
+        buttonDelet.addEventListener('click', e => {
+            const indexToRemove = e.target.parentElement.dataset.index;
+            myLibrary.splice(indexToRemove, 1);
+            content.innerHTML = '';
+            books();
+        })
+
         for (let property in myLibrary[i]) {
             let p = document.createElement('p');
             p.textContent = `${property} : ${myLibrary[i][property]}\n`;
             book.appendChild(p);
-            book.appendChild(yes);
-            book.appendChild(not);
         }
-
+        book.appendChild(buttonDelet);
         content.appendChild(book);
-        myLibrary.length = 0;
     }
 }
 
@@ -56,7 +66,6 @@ save.addEventListener('click', (e)=> {
     
     if (verification(title) && verification(author) && verification(pages)) {
         addBookToLibrary(title, author, pages);
-        books();
     } 
 
     dialog.close();
