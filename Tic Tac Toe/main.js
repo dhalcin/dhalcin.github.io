301de58cdfebdebe6dfb40c6e58d944cf3bc3dbc
jhon <hankgit-h@hotmail.com>
2024-03-27 11:49:51 -0500

    update

--- Tic Tac Toe/main.js
+++ Tic Tac Toe/main.js
@@ -46,21 +46,33 @@ const GameController = (() => {
         }
     };
 
+    /* Array containing possible winning combinations in tic-tac-toe game.
+       Each subarray represents a winning combination, where each element is
+       a coordinate [row, column] on the game board*/
+
     const winConditions = [
-        [[0, 0], [0, 1], [0, 2]],
-        [[1, 0], [1, 1], [1, 2]],
-        [[2, 0], [2, 1], [2, 2]],
-        [[0, 0], [1, 0], [2, 0]],
-        [[0, 1], [1, 1], [2, 1]],
-        [[0, 2], [1, 2], [2, 2]],
-        [[0, 0], [1, 1], [2, 2]],
-        [[0, 2], [1, 1], [2, 0]]
+        // Winning combinations by rows
+        [[0, 0], [0, 1], [0, 2]], // First row
+        [[1, 0], [1, 1], [1, 2]], // Second row
+        [[2, 0], [2, 1], [2, 2]], // Third row
+
+        // Winning combinations by columns
+        [[0, 0], [1, 0], [2, 0]], // First column
+        [[0, 1], [1, 1], [2, 1]], // Second column
+        [[0, 2], [1, 2], [2, 2]], // Third column
+
+        // Winning combinations by diagonals
+        [[0, 0], [1, 1], [2, 2]], // Main diagonal
+        [[0, 2], [1, 1], [2, 0]]  // Secondary diagonal
     ];
 
     const checkWin = () => {
         for (let condition of winConditions) {
+            // Extact symbols from the game board based on the current condition
             let symbols = condition.map(([row, col]) => Gameboard.board[row][col]);
             let fristSymbol = symbols[0];
+
+            // Check if all symbols in the current condition are the same
             if (fristSymbol && symbols.every(symbol => symbol === fristSymbol)) {
                 return fristSymbol === 'X' ? player1 : player2;
             }
@@ -75,10 +87,14 @@ const GameController = (() => {
         return { row, col };
     }
     
+    // Function to verify if a cell is empty or already occupied
     const verification = (row, col) => {
         let elemnt = Gameboard.board[row][col];
+
+        // Iterate through each cell on the game board
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
+                // Check if the current cell matches the specified row and column, and if it's already occupied
                 if ((row === i && col === j) && (elemnt === 'X' || elemnt === '0')) return false;
             }
         } 
@@ -88,7 +104,7 @@ const GameController = (() => {
     const handleWin = () => {
         let winner = checkWin();
         if (winner) {
-            console.log(winner === 'draw' ? 'Empate' : `¡${winner.name} has won the game!`);
+            console.log(winner === 'draw' ? 'draw' : `¡${winner.name} has won the game!`);
             return true;
         }
         return false;
@@ -97,12 +113,15 @@ const GameController = (() => {
 
     const makeMove = () => {
         while (true) {
+            // Get the coordinate selected by the player
             let { row, col } = coord();
             
+            // Check if the coordinates are valid
             if (!verification(row, col)) continue;
     
             addSymbol(row, col);
     
+            // Check for a winner after adding the symbol
             if (handleWin()) break;
     
             switchPlayer();
