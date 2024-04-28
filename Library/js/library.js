7de751549277b073d54ccf03dffba57cdcbe4239
jhon <hankgit-h@hotmail.com>
2024-04-27 21:46:54 -0500

    Implementation of the use of classes in the library project.

--- Library/js/library.js
+++ Library/js/library.js
@@ -1,126 +1,59 @@
-
-const add = document.getElementById('add');
-const dialog = document.getElementById('dialog');
-const save = document.getElementById('save');
-const closed = document.getElementById('closed');
-const content = document.getElementsByClassName('content')[0];
-
-const myLibrary = [];
-
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = false;
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
 }
 
-function verification(str) {
-    return (str).trim() !== '';
-}
+class UI {
+    add(book) {
+        const element = document.createElement('div');
+        element.innerHTML = `
+                <strong>Title</strong>: ${book.title}
+                <strong>Author</strong>: ${book.author}
+                <strong>Pages</strong>: ${book.pages}
+                <button name='delete'>Delete</button>
+        `;
 
-function renderBooks() {
-    content.innerHTML = '';
-    books();
-}
-
-function addBookToLibrary(title, author, pages) {
-    let book = new Book(title, author, pages);
-    myLibrary.push(book);
-    renderBooks();
-}
-
-function books() {
-   for (let i = 0; i < myLibrary.length; i++) {
-    
-    // Creating container
-    let contentBook = document.createElement('div');
-    contentBook.classList.add('contentBook');
-    
-    // Adding the book container
-    let book = document.createElement('div');
-    book.dataset.index = i;
-    book.classList.add('book-container');
-
-    // Adding the buttons container
-    let contentButton = document.createElement('div');
-    contentButton.classList.add('buttons-container');
-
-    let readButton = document.createElement('button');
-    readButton.classList.add('status')
-    readButton.textContent = myLibrary[i].read ? 'Unread' : 'Read';
-
-    let buttonDelet = document.createElement('button');
-    buttonDelet.classList.add('delet');
-    buttonDelet.innerHTML = '<img src="dhalcin.github.io/Library/images/trash.svg" alt="trash">';
-
-    for (let property in myLibrary[i]) {
-        // Formatting : Title, Author and Pages
-        let p = document.createElement('p');
-        let format = property;
-        
-        switch (format) {
-            case 'title':
-                p.textContent = `Title : ${myLibrary[i][property]}\n`;
-                break;
-            
-            case 'author':
-                p.textContent = `Author : ${myLibrary[i][property]}\n`;
-                break;
-            
-            case 'pages':
-                p.textContent = `Pages : ${myLibrary[i][property]}\n`;
-        }
-
-        book.appendChild(p);
+        content.appendChild(element);
     }
 
-    readButton.addEventListener('click', ()=> {
-        myLibrary[i].read = !myLibrary[i].read;
-        readButton.textContent = myLibrary[i].read ? 'Unread' : 'Read';
-        readButton.classList.toggle('unread');
-    })
+    delete(element) {
+        if (element.name === 'delete') {
+            element.parentElement.remove()
+            this.showMessage();
+        }
+    }
 
-    buttonDelet.addEventListener('click', e => {
-        const indexToRemove = e.target.parentElement.dataset.index;
-        myLibrary.splice(indexToRemove, 1);
-        content.innerHTML = '';
-        books();
-    })
-
-    contentBook.appendChild(book);
-    contentButton.appendChild(readButton);
-    contentButton.appendChild(buttonDelet);
-    contentBook.appendChild(contentButton);
-
-    content.appendChild(contentBook);
-   }
-
+    showMessage() {
+        alert('Libro borrado')
+    }
 }
 
-add.addEventListener('click', ()=> {
-    dialog.showModal(); 
-})
+const dialog = document.getElementById('dialog');
+const content = document.querySelector('.content');
 
-save.addEventListener('click', (e)=> {
-    e.preventDefault();
-    let titleInput = document.getElementById('title-book');
-    let authorInput = document.getElementById('author');
-    let pagesInput = document.getElementById('pages');
-    
-    let title = titleInput.value;
-    let author = authorInput.value;
-    let pages = pagesInput.value;
-    
-    if (verification(title) && verification(author) && verification(pages)) {
-        addBookToLibrary(title, author, pages);
-        titleInput.value = '';
-        authorInput.value = '';
-        pagesInput.value = '';  
-    } 
-    dialog.close();
-})
+document.getElementById('add').addEventListener('click', ()=> {
+    dialog.showModal();
+});
 
-closed.addEventListener('click', e => {
-    e.preventDefault();
-    dialog.close();
+document.getElementById('save').addEventListener('click', ()=> {
+    const title = document.getElementById('title-book').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+
+    if (title && author && pages) {
+        const book = new Book(title, author, pages);
+        const ui = new UI();
+        ui.add(book);
+        dialog.close();
+    } else {
+        alert('completa');
+    }
+});
+
+content.addEventListener('click', (e)=> {
+    const ui = new UI();
+    ui.delete(e.target);
 })
