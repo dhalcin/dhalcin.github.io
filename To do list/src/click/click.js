a058d8e660a80d1b367983839e6241ea447341c3
jhon <hankgit-h@hotmail.com>
2024-06-06 18:22:02 -0500

    Update file

--- To do list/src/click/click.js
+++ To do list/src/click/click.js
@@ -1,14 +1,17 @@
 import AddTask from "../task/task";
+import Modal from "../modal/modal";
 
 export default class Button {
     constructor(button) {
         this.button = button;
-        this.task = new AddTask();
+        //this.task = new AddTask();
+        this.modal = new Modal();
     }
 
     eventClick() {
         this.button.addEventListener('click', () => {
-            this.task.addDiv();   
+            //this.task.addDiv();   
+            this.modal.getModal();
         });
     }
 }
