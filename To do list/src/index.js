9a79d7d96fba018249abfff811ec799558c76ac1
jhon <hankgit-h@hotmail.com>
2024-06-07 21:36:35 -0500

    Set date using date-fns

--- To do list/src/index.js
+++ To do list/src/index.js
@@ -1,7 +1,11 @@
 import './style.css';
-import Button from './click/click'
+import { format } from 'date-fns';
+import Button from './click/click';
 
 const addBtn = document.getElementById('addBtn');
-
 const add = new Button(addBtn);
 add.eventClick();
+
+const date = document.getElementById('dueDate');
+date.value = format(new Date(), 'yyyy-MM-dd');
+
