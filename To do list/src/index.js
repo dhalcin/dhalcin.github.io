43f14ef4b8275cdeee9a3bc867e49961bf567494
jhon <hankgit-h@hotmail.com>
2024-06-10 21:39:10 -0500

    Update file

--- To do list/src/index.js
+++ To do list/src/index.js
@@ -3,9 +3,16 @@ import { format } from 'date-fns';
 import Button from './click/click';
 
 const addBtn = document.getElementById('addBtn');
-const add = new Button(addBtn);
-add.eventClick();
-
 const date = document.getElementById('dueDate');
 date.value = format(new Date(), 'yyyy-MM-dd');
 
+const add = new Button(addBtn);
+add.eventClick('add');
+
+const closedBtn= document.getElementById('closed-btn');
+const closed = new Button(closedBtn);
+closed.eventClick('closed')
+
+const saveBtn = document.getElementById('save-btn');
+const save = new Button(saveBtn);
+save.eventClick('save');
