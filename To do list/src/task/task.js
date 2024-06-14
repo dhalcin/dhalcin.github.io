8e1ae312501f4a837864e5a4c641f1e826c21dca
jhon <hankgit-h@hotmail.com>
2024-06-14 16:30:57 -0500

    Refactoring the code

--- To do list/src/task/task.js
+++ To do list/src/task/task.js
@@ -1,3 +1,5 @@
+import Element from "../element/element";
+
 export default class AddTask {
     constructor() {
         this.container = document.querySelector('.tasks');
@@ -11,54 +13,55 @@ export default class AddTask {
         const task = document.createElement('div');
         task.classList.add('task');
 
-        const titleTask = document.createElement('h3');
-        titleTask.classList.add('task-title');
-        titleTask.textContent = `${this.taskName.value}`;
-        task.appendChild(titleTask);
+        for (let e = 0; e < 6; e++) {
+            const taskDiv = document.createElement('div');
+            taskDiv.classList.add('task-div');
+            task.appendChild(taskDiv);
+        }
 
-        const taskDescription = document.createElement('div');
-        taskDescription.textContent = `${this.description.value}`;
-        task.appendChild(taskDescription);
+        const childContainer = this.container.children;
 
-        const priority = document.createElement('div');
+        this.container.appendChild(task);
+
+        const lastTask = childContainer[childContainer.length - 1];
+        const lastChild = lastTask.children;
+
+        lastChild[0].textContent = `${this.taskName.value}`;
+        lastChild[1].classList.add('text-description');
+        lastChild[1].textContent = `${this.description.value}`;
+
         for (let element of this.inpts) {
             if (element.checked) {
-                const btn = document.createElement('button');
-                btn.classList.add('button-task');
+                const button = document.createElement('button');
+                button.classList.add('button-task');
+
                 const i = document.createElement('i');
                 i.classList.add('bi-circle');
+                
+                if (element.id === 'low') i.style.color = 'yellow';
+                if (element.id === 'medium') i.style.color = 'blue';
+                if (element.id === 'high') i.style.color = 'red';
 
-                btn.appendChild(i);
-                priority.appendChild(btn);
+                button.appendChild(i);
+                lastChild[2].appendChild(button);
             }
         }
 
-        task.appendChild(priority);
+        lastChild[3].textContent = `${this.dueDate.value}`;
+        const btn1 = document.createElement('button');
+        btn1.classList.add('button-task');
+        const i1 = document.createElement('i');
+        i1.classList.add('bi-pencil');
+        btn1.appendChild(i1);
+        
+        lastChild[4].appendChild(btn1);
+    
+        const btn2 = document.createElement('button');
+        btn2.classList.add('button-task');
+        const i2 = document.createElement('i');
+        i2.classList.add('bi-trash3');
+        btn2.appendChild(i2);
 
-        const taskDate = document.createElement('div');
-        taskDate.textContent = `${this.dueDate.value}`
-        task.appendChild(taskDate);
-
-        const edit = document.createElement('div');
-        const editBtn = document.createElement('button');
-        editBtn.classList.add('button-task');
-        const editI = document.createElement('i');
-        editI.classList.add('bi-pencil');
-
-        editBtn.appendChild(editI);
-        edit.appendChild(editBtn);
-        task.appendChild(edit);
-
-        const delet = document.createElement('div');
-        const delBtn = document.createElement('button');
-        delBtn.classList.add('button-task');
-        const delI = document.createElement('i');
-        delI.classList.add('bi-trash3');
-
-        delBtn.appendChild(delI);
-        delet.appendChild(delBtn);
-        task.appendChild(delet);
-
-        this.container.appendChild(task);
+        lastChild[5].appendChild(btn2);
     }
 }
