9091e23a3b155c8e018d5392a029438198d142d6
jhon <hankgit-h@hotmail.com>
2024-06-16 21:27:35 -0500

    Refactoring the code

--- To do list/src/task/task.js
+++ To do list/src/task/task.js
@@ -1,5 +1,3 @@
-import Element from "../element/element";
-
 export default class AddTask {
     constructor() {
         this.container = document.querySelector('.tasks');
@@ -9,59 +7,72 @@ export default class AddTask {
         this.description = document.getElementById('description');
     }
 
-    addDiv() {
-        const task = document.createElement('div');
-        task.classList.add('task');
+    validateInputs() {
+        return this.taskName.value !== '' && this.description.value !== '';
+    }
 
+    createButton(iconClass, color = null) {
+        const button = document.createElement('button');
+        button.classList.add('button-task');
+
+        const icon = document.createElement('i');
+        icon.classList.add(iconClass);
+
+        if (color) icon.style.color = color;    
+
+        button.appendChild(icon);
+        return button;
+    }
+
+    createTaskDiv(task) {
         for (let e = 0; e < 6; e++) {
             const taskDiv = document.createElement('div');
-            taskDiv.classList.add('task-div');
             task.appendChild(taskDiv);
         }
+    }
 
-        const childContainer = this.container.children;
-
-        this.container.appendChild(task);
-
-        const lastTask = childContainer[childContainer.length - 1];
-        const lastChild = lastTask.children;
-
-        lastChild[0].textContent = `${this.taskName.value}`;
-        lastChild[1].classList.add('text-description');
-        lastChild[1].textContent = `${this.description.value}`;
-
+    setPriority(lastChild) {
         for (let element of this.inpts) {
             if (element.checked) {
-                const button = document.createElement('button');
-                button.classList.add('button-task');
-
-                const i = document.createElement('i');
-                i.classList.add('bi-circle');
-                
-                if (element.id === 'low') i.style.color = 'yellow';
-                if (element.id === 'medium') i.style.color = 'blue';
-                if (element.id === 'high') i.style.color = 'red';
-
-                button.appendChild(i);
-                lastChild[2].appendChild(button);
+                let color;
+                if (element.id === 'low') color = 'yellow';
+                if (element.id === 'medium') color = 'blue';
+                if (element.id === 'high') color = 'red';
+                const priorityButton = this.createButton('bi-circle', color);
+                lastChild[2].appendChild(priorityButton);
             }
         }
+    }
 
-        lastChild[3].textContent = `${this.dueDate.value}`;
-        const btn1 = document.createElement('button');
-        btn1.classList.add('button-task');
-        const i1 = document.createElement('i');
-        i1.classList.add('bi-pencil');
-        btn1.appendChild(i1);
+
+    addDiv() {
+        if (!this.validateInputs()) {
+            return false;
+        }
+
+        const task = document.createElement('div');
+        task.classList.add('task');
+        this.createTaskDiv(task);
+        this.container.appendChild(task);
+
+        const lastTask = this.container.lastElementChild;
+        const lastChild = lastTask.children;
+
+        lastChild[0].textContent = this.taskName.value;
+        lastChild[1].classList.add('text-description');
+        lastChild[1].textContent = this.description.value;
         
-        lastChild[4].appendChild(btn1);
-    
-        const btn2 = document.createElement('button');
-        btn2.classList.add('button-task');
-        const i2 = document.createElement('i');
-        i2.classList.add('bi-trash3');
-        btn2.appendChild(i2);
+        this.setPriority(lastChild);
+        lastChild[3].textContent = this.dueDate.value;
 
-        lastChild[5].appendChild(btn2);
+        const btnPencil = this.createButton('bi-pencil');
+        lastChild[4].appendChild(btnPencil);
+
+        const btnTrash = this.createButton('bi-trash3');
+        lastChild[5].appendChild(btnTrash);
+
+        this.taskName.value = '';
+        this.description.value = '';
+        return true;
     }
 }
