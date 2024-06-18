2a49b4d6c5315916626ed72e434c69f3d682dfb7
jhon <hankgit-h@hotmail.com>
2024-06-17 21:45:53 -0500

    update import and instances to use Event module

--- To do list/src/index.js
+++ To do list/src/index.js
@@ -1,23 +1,23 @@
 import './style.css';
 import { format } from 'date-fns';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Button from './click/click';
+import Event from './events/events';
 import DateModule from './date/date';
 
 const addBtn = document.getElementById('addBtn');
 const date = document.getElementById('dueDate');
 date.value = format(new Date(), 'yyyy-MM-dd');
 
-const add = new Button(addBtn);
-add.eventClick('add');
+const add = new Event(addBtn);
+add.eventHandler('add');
 
 const closedBtn= document.getElementById('closed-btn');
-const closed = new Button(closedBtn);
-closed.eventClick('closed')
+const closed = new Event(closedBtn);
+closed.eventHandler('closed')
 
 const saveBtn = document.getElementById('save-btn');
-const save = new Button(saveBtn);
-save.eventClick('save');
+const save = new Event(saveBtn);
+save.eventHandler('save');
 
 const taskDate = document.querySelector('.task-duedate');
 const newDate = new DateModule();
