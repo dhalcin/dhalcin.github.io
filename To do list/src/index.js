b5527fd58e0816892afce31d97ad6d2da07fe2aa
jhon <hankgit-h@hotmail.com>
2024-06-21 19:03:54 -0500

    Refactor event initialization and handling in index.js
    - Consolidated event handling into a single Events instance.
    - Removed redundant button-specific Event instances and handlers.
    - Simplified event binding by initializing and invoking Events.click() once.
    - Ensured proper initialization of dueDate and taskDate elements.
    - Improved code clarity and maintainability by reducing repetitive code.

--- To do list/src/index.js
+++ To do list/src/index.js
@@ -1,23 +1,14 @@
 import './style.css';
 import { format } from 'date-fns';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Event from './events/events';
+import Events from './events/events';
 import DateModule from './date/date';
 
-const addBtn = document.getElementById('addBtn');
 const date = document.getElementById('dueDate');
 date.value = format(new Date(), 'yyyy-MM-dd');
 
-const add = new Event(addBtn);
-add.eventHandler('add');
-
-const closedBtn= document.getElementById('closed-btn');
-const closed = new Event(closedBtn);
-closed.eventHandler('closed')
-
-const saveBtn = document.getElementById('save-btn');
-const save = new Event(saveBtn);
-save.eventHandler('save');
+const events = new Events();
+events.click();
 
 const taskDate = document.querySelector('.task-duedate');
 const newDate = new DateModule();
