24c5dfbd6e6f0679a09402dcfc9a2396d0ffee2e
jhon <hankgit-h@hotmail.com>
2024-06-23 18:11:30 -0500

    Refactor: Simplify initialization logic and remove redundant date setup
    - Removed redundant date initialization logic from index.js
    - Moved date setup logic to DateModule for better encapsulation
    - Ensured date values are correctly set for the default task and modal input
    - Improved code structure by initializing the Events instance

--- To do list/src/index.js
+++ To do list/src/index.js
@@ -1,15 +1,6 @@
 import './style.css';
-import { format } from 'date-fns';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Events from './events/events';
-import DateModule from './date/date';
-
-const date = document.getElementById('dueDate');
-date.value = format(new Date(), 'yyyy-MM-dd');
 
 const events = new Events();
 events.click();
-
-const taskDate = document.querySelector('.task-duedate');
-const newDate = new DateModule();
-taskDate.textContent = `${format(newDate.date, 'dd-MM-yyyy')}`;
