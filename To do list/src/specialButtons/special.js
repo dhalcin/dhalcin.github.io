6ad83026866212004d46008c35c02db2f902d97d
jhon <hankgit-h@hotmail.com>
2024-06-22 21:41:05 -0500

    Updated Special.js to enhance the edit() method
    In this update to Special.js:
    - Expanded the edit() method to accept additional parameters (name, description, priority) and assign these values to corresponding elements in the user interface.
    - Added conditions to manage task priority based on the selected icon.

--- To do list/src/specialButtons/special.js
+++ To do list/src/specialButtons/special.js
@@ -4,13 +4,30 @@ export default class Special {
         this.container = element.closest('.task');
     }
 
-    edit() {
+    edit(nameInput, descriptionInput, prioritysInpt) {
         // Get the task name element within the task container
-        const taskName = (this.container).querySelector('.task-name h3');
+        const taskName = this.container.querySelector('.task-name h3');
+
+        nameInput.value = taskName.textContent;
+
+        // Get the `i` element representing the task priority within the task container
+        const priorityIcon = this.container.querySelector('.bi-circle');
+
+        // Retrieve the `dataset` from the `i` element
+        const priority = priorityIcon.dataset.priority;
+
+        
+        if (priority === 'low') prioritysInpt[0].checked = true;
+
+        if (priority === 'medium') prioritysInpt[1].checked = true;
+
+        if (priority === 'high') prioritysInpt[2].checked = true;
 
         // Get the task description element within the task container
-        const taskDescription = (this.container).querySelector('.text-description');
-        console.log(taskName.textContent, taskDescription.textContent);
+        const taskDescription = this.container.querySelector('.text-description');
+        
+        descriptionInput.value = taskDescription.textContent;
+
     }
 
     trash() {
