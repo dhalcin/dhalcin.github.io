23336ba1109a66fee8f73d5f87c6dd8259409421
jhon <hankgit-h@hotmail.com>
2024-06-26 21:33:58 -0500

    Getting the value of the date input (modal) and giving it a suitable format

--- To do list/src/specialButtons/special.js
+++ To do list/src/specialButtons/special.js
@@ -1,21 +1,28 @@
+import DateModule from "../date/date";
+
 export default class Special {
     // Constructor accepts an element and finds the closest '.task' container
-    constructor(element) {
+    constructor(element, date) {
+        this.date = date;
         this.container = element.closest('.task');
     }
 
-    edit(nameInput, descriptionInput, prioritysInpt) {
+    edit(nameInput, descriptionInput, dateInpt, prioritysInpt) {
         // Get the task name element within the task container
         const taskName = this.container.querySelector('.task-name h3');
 
         nameInput.value = taskName.textContent;
+
+        const taskDate = this.container.querySelector('.task-duedate');
+
+        // Get the date of the task, giving it a correct format and adding it to the modal
+        dateInpt.value = this.date.ParseIso(taskDate.textContent);
 
         // Get the `i` element representing the task priority within the task container
         const priorityIcon = this.container.querySelector('.bi-circle');
 
         // Retrieve the `dataset` from the `i` element
         const priority = priorityIcon.dataset.priority;
-
         
         if (priority === 'low') prioritysInpt[0].checked = true;
 
