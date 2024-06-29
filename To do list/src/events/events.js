7b8cfb732ec7bf60ea0355622a63e500e0204d98
jhon <hankgit-h@hotmail.com>
2024-06-28 22:06:08 -0500

    Managing the editing of a select task

--- To do list/src/events/events.js
+++ To do list/src/events/events.js
@@ -8,6 +8,10 @@ export default class Events {
         this.modal = new Modal();
         this.date = new DateModule();
         this.task = new AddTask(this.date);
+        this.special = null;
+        
+        // Setting a flag
+        this.flag = null;
     }
 
     // Method to verify if the task can be added successfully
@@ -30,9 +34,6 @@ export default class Events {
             
             let element = e.target;
             
-            // Entering 'Special' constructor properties: Task container and DateModule class (this.date)
-            const special = new Special(element, this.date);
-
             switch (element.id) {
                 case 'addBtn':
                     this.modal.openModal();
@@ -48,6 +49,16 @@ export default class Events {
                 
                 // Save task and close modal when the save button is clicked, if verification is successful
                 case 'save-btn':
+
+                    // Get the true os 'flag' and add edits to the current task, using 'trash()' and immediately adding the new 'edited task'
+                    if (this.flag && this.verification()) {
+
+                        // Since there is already an instance of 'Special' you can use 'trash()'
+                        this.special.trash();
+                        this.modal.closedModal();
+                        this.flag = null;
+                    }
+
                     if (this.verification()) this.modal.closedModal();
                     break;
 
@@ -61,15 +72,20 @@ export default class Events {
             
             // Check if the clicked element is the edit button (pencil icon)
             if ((element.classList).contains('bi-pencil')) {
+
+                // An instance of 'Special' is only created when the dit button is clicked
+                this.special = new Special(element, this.date);
                 this.modal.openModal();
 
                 // Entering the arguments task name, task description, task date, task priority
-                special.edit(this.task.taskName, this.task.description, this.task.dueDate, this.task.inpts);
+                this.special.edit(this.task.taskName, this.task.description, this.task.dueDate, this.task.inpts);
+                this.flag = true;
                 
             }
 
             // Check in the clicked element is delete button (trash icon)
             if (element.classList.contains('bi-trash3')) {
+                const special = new Special(element, this.date);
                 special.trash();
             }
 
