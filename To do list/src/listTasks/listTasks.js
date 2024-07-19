8834bf43252c62945ffe8999a242a1e38cff9c32
jhon <hankgit-h@hotmail.com>
2024-07-18 23:11:53 -0500

    Adding method 'remeberTask()'

--- To do list/src/listTasks/listTasks.js
+++ To do list/src/listTasks/listTasks.js
@@ -1,8 +1,35 @@
 export default class List {
     constructor() {
+        this.container = document.querySelector('.tasks');
+        this.child = this.container.children;
         this.low = [];
         this.medium = [];
         this.high = [];
+    }
+
+    remeberTask() {
+        for (let i = 1; i < this.child.length; i++) {
+            const task = this.child[i];
+            const priority = task.querySelector('.bi-circle').dataset.priority;
+
+            switch (priority) {
+                case 'low':
+                    this.lowPriority(task);
+                    break;
+                
+                case 'medium':
+                    this.mediumPriority(task);
+                    break;
+                
+                case 'high':
+                    this.highPriority(task);
+                    break;
+                
+                default:
+                    break;
+            }
+           
+        }
     }
 
     lowPriority(task) {
