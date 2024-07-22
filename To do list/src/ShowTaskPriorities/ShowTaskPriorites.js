ea45dcbc20751a6ad2853a694374c34651dad613
jhon <hankgit-h@hotmail.com>
2024-07-22 16:41:00 -0500

    Refactor: Rename class and modules
    - Renamed class 'Show' to 'ShowTaskPriorities'.
    - Update module 'showTasks/showTasks.js' to 'ShowTaskPriorities/ShowTaskPriorites.js'.
    - Adjusted all references to the renamed class and modules.
    - Ensured all tests pass with the new names

--- To do list/src/ShowTaskPriorities/ShowTaskPriorites.js
+++ To do list/src/ShowTaskPriorities/ShowTaskPriorites.js
@@ -1,18 +1,21 @@
-export default class Show {
+export default class ShowTaskPriorities {
     constructor(list) {
         // * container task -> div.tasks
         this.container = document.querySelector('.tasks');
         this.nodes = this.container.children;
+
+        // 'list' argument initializing ListPriorities in event.js
         this.list = list;
     }
 
+    // delete all children of div.tasks; except div.legend
     emptyContainer() {
-        console.log(this.nodes);
         while (this.nodes.length > 1) {
             this.nodes[1].remove();
         }
     }
 
+    // adding the tasks to this.container
     appendTask(list) {
         for (let i = 0; i < list.length; i++) {
             this.container.appendChild(list[i]);
