d4dc5f4135fe019c2927b23e5bf540355a862e1a
Jhon <hankgit-h@hotmail.com>
2024-07-21 21:39:25 -0500

    Creating the logic for localStorage

---
+++ To do list/src/storage/storage.js
@@ -0,0 +1,30 @@
+export default class Storage {
+    constructor() {
+        this.tasks = [];
+    }
+    
+    store() {
+        return localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+
+    getSave(nameTask, description , priority, date) {
+        const storedTask = localStorage.getItem('tasks');
+        
+        let task = {
+            "name": nameTask,
+            "description": description,
+            "priority": priority,
+            "date": date
+        }
+
+        if (!storedTask) {
+            this.tasks.push(task);
+            this.store();
+
+        } else {
+            this.tasks = JSON.parse(storedTask);
+            this.tasks.push(task);
+            this.store();
+        }
+    }
+}
