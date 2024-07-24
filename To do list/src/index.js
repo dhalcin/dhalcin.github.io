dc73c8217be0858a607a8fd7c4171983139c8d98
jhon <hankgit-h@hotmail.com>
2024-07-23 22:15:49 -0500

    Updated index.js to handle tasks stored in localStorage
    - Added handling of tasks stored in localStorage when initializing
      Events.
    
    - Convert JSON string of localStorage to array or initialize as empty if
      there are no tasks.

--- To do list/src/index.js
+++ To do list/src/index.js
@@ -2,6 +2,11 @@ import './style.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Events from './events/events';
 
-const events = new Events();
-events.click();
-events.remeber();
+document.addEventListener('DOMContentLoaded', () => {
+    // Gets the tasks stored in localStorage and converts them from JSON to an array
+    // If there are no stored tasks, it is initialized as an empty array
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const events = new Events(storedTasks);
+    events.click();
+    events.remeber();
+});
