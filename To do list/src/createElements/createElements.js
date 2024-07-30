a552091e5645910f5d9eb7b78c85327fde43811e
jhon <hankgit-h@hotmail.com>
2024-07-29 21:02:32 -0500

    Removed and created new methodos:
    - Removed the 'createElement' method and replace it with more specific
      methodos.
    
    - Added logic for the creation of nodes such as: div, h3, p, span and
      buttons.

--- To do list/src/createElements/createElements.js
+++ To do list/src/createElements/createElements.js
@@ -1,19 +1,41 @@
 export default class Create {
-    constructor() {
-        this.element = null;
-    }
-
-    elements(tag, className, content = null) {
-        let element = document.createElement(tag);
+    // Method to create a div
+    div(className, container) {
+        let element = document.createElement('div');
         element.classList.add(className);
-        
-        if (content) element.textContent = content;
-
-        return element;
+        container.appendChild(element);
+        return element
     }
 
-    createElement(tag, className, content) {
-        this.element = this.elements(tag, className, content);
-        console.log(this.element);
+    // Method to create an h3, p or span element
+    h3_P_Span(tag, content, container) {
+        let element = document.createElement(tag);
+        if (tag === 'h3') element.classList.add('task-name');
+        if (tag === 'p') element.classList.add('text-description');
+        if (tag === 'span') element.classList.add('task-duedate');
+
+        element.textContent = content;
+        container.appendChild(element);
+    }
+
+    // Method for creating buttons
+    buttons(typeButton, priority = null, container) {
+        let element = document.createElement('button');
+        element.classList.add('button-task');
+        let icon = document.createElement('i');
+
+        // * Dependig on the type of button; bi-circle, bi-pencil, bi-trash and taking into account the button priority (if any)
+        if (typeButton === 'bi-circle') {
+            icon.classList.add(typeButton);
+            if (priority === 'low') icon.style.color = 'yellow';
+            if (priority === 'medium') icon.style.color = 'blue';
+            if (priority === 'high') icon.style.color = 'red';
+
+        } else {
+            icon.classList.add(typeButton);
+        }
+
+        element.appendChild(icon);
+        container.appendChild(element);
     }
 }
